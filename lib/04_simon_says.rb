e372feb530da07fc323d96ca858fe58d2ddd767c
thomas.bobichon@hotmail.fr <thomas.bobichon@hotmail.fr>
2024-04-08 17:18:14 +0200

    updates

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -7,17 +7,15 @@ def shout(string)
     return cap_string
 end
 
-def repeat(*string)
-    return string
-end
-
 def repeat(text, n=2)
     return Array.new(n, text).join(' ')
 end
 
+
 def start_of_word(str, index)
     return str[0, index]
 end
+
 
 def first_word(string)
     words = string.split(/\s+/)
--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,15 +1,16 @@
 def translate(words)
-    translated_words = words.split.map do |word|
-      if word.start_with?('a', 'e', 'i', 'o', 'u')
-        word + 'ay'
-      else
-        first_vowel_index = word.index(/[aeiou]/)
-        if first_vowel_index == 1 && word[0..1] == "qu"
-          word[2..-1] + word[0..1] + 'ay'
-        else
-          word[first_vowel_index..-1] + word[0...first_vowel_index] + 'ay'
-        end
+  translated_words = words.split.map do |word|
+    if word.start_with?('a', 'e', 'i', 'o', 'u')
+      word + 'ay'
+    else
+      first_vowel_index = word.index(/[aeiou]/)
+      if word.start_with?('qu')
+        first_vowel_index = 2
+      elsif word[1..2] == "qu"
+        first_vowel_index = 3
       end
+      word[first_vowel_index..-1] + word[0...first_vowel_index] + 'ay'
     end
-    return translated_words.join(' ')
   end
+  return translated_words.join(' ')
+end
